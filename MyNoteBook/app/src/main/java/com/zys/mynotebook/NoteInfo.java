b6ccc230d21a05a750b3d86ef02dd1d59b6c77ce
Jun611 <56495467+Jun611@users.noreply.github.com>
2020-11-30 22:23:53 +0800

    second

---
+++ MyNoteBook/app/src/main/java/com/zys/mynotebook/NoteInfo.java
@@ -0,0 +1,51 @@
+package com.zys.mynotebook;
+
+import java.io.Serializable;
+
+public class NoteInfo implements Serializable {
+    private String id;
+    private String title;
+    private String content;
+    private String author;
+    private String date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
